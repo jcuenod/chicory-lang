c4d412ef999d31530deab13d45504fbfbb69780e
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-02-12 20:49:54 -0600

    Refactor test/compile

--- compile.ts
+++ compile.ts
@@ -3,15 +3,9 @@ import { ChicoryLexer } from './generated/ChicoryLexer';
 import { ChicoryParser } from './generated/ChicoryParser';
 import { ChicoryParserListener } from './ChicoryListener';
 
-const tests = [
-    './tests/1. assignments'
-]
-
 const listener = new ChicoryParserListener();
 
-tests.forEach(async path => {
-    console.log(`Running test: ${path}`);
-    const {source, compiled} = await import(path)
+export default (source: string) => {
     let inputStream = CharStream.fromString(source);
     let lexer = new ChicoryLexer(inputStream);
     let tokenStream = new CommonTokenStream(lexer);
@@ -20,16 +14,5 @@ tests.forEach(async path => {
     
     ParseTreeWalker.DEFAULT.walk(listener, tree);
     
-    const compiledOutput = listener.getOutput();
-
-    if (compiledOutput.trim() === compiled.trim()){
-        console.log(' - Test passed\n');
-    }
-    else {
-        console.log(' - Test failed');
-        console.log(' - Expected:');
-        console.log(compiled);
-        console.log(' - Got:');
-        console.log(compiledOutput + '\n');
-    }
-})
+    return listener.getOutput();
+}
---
+++ test.ts
@@ -0,0 +1,22 @@
+import compile from "./compile";
+
+const tests = [
+    './tests/1. assignments'
+]
+
+tests.forEach(async path => {
+    console.log(`Running test: ${path}`);
+    const {source, compiled} = await import(path)
+    const compiledOutput = compile(source);
+
+    if (compiledOutput.trim() === compiled.trim()){
+        console.log(' - Test passed\n');
+    }
+    else {
+        console.log(' - Test failed');
+        console.log(' - Expected:');
+        console.log(compiled);
+        console.log(' - Got:');
+        console.log(compiledOutput + '\n');
+    }
+})
