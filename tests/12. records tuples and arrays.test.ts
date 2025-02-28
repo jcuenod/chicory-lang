320f7781238665baebb28299e128057ceaf7ccc3
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-02-28 15:44:47 -0600

    Add failing records, arrays, and tuples test

---
+++ tests/12. records tuples and arrays.test.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "bun:test";
+import compile from "../compile";
+
+test("assign records", () => {
+  const { code } = compile(`const rec = { a: "one", b: true, c: 3, d: four, }`);
+  expect(code).toBe(`const rec = { a: "one", b: true, c: 3, d: four };`);
+});
+
+test("assign tuples", () => {
+  const { code } = compile(`const tup = ["one", true, 3]`);
+  expect(code).toBe(`const tup = ["one", true, 3];`);
+});
+
+test("assign arrays", () => {
+  const { code } = compile(`const arr = ["one", "two", "three"]`);
+  expect(code).toBe(`const arr = ["one", "two", "three"];`);
+});
+
+test("return record in if expr", () => {
+  const { code } = compile(`if (true) { a: "one", b: true, c: 3, d: four }`);
+  expect(code).toBe(
+    `(() => { if (true) { return { a: "one", b: true, c: 3, d: four }; }})();`
+  );
+});
+
+test("return record in match arm", () => {
+  const { code } = compile(`match (str) { 
+    "hello" => { a: "one", b: true, c: 3, d: four },
+    _ => { { a: "one", b: true, c: 3, d: four } }
+}`);
+  expect(code).toBe(`(() => {
+    const __chicory_var_0 = str;
+    if (__chicory_var_0 === "hello") {
+        return { a: "one", b: true, c: 3, d: four };
+    }
+    else if (true) {
+        return { a: "one", b: true, c: 3, d: four };
+    }
+})();`);
+});
