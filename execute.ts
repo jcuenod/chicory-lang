367e323955319560d6390b2c912c87ab2cb5a727
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-02-28 15:03:21 -0600

    fmt

--- execute.ts
+++ execute.ts
@@ -1,31 +1,34 @@
-import compile from "./compile"
+import compile from "./compile";
 
-const yellowBoldTermPrefix = '\x1b[33m\x1b[1m';
-const resetStyle = '\x1b[0m';
+const yellowBoldTermPrefix = "\x1b[33m\x1b[1m";
+const resetStyle = "\x1b[0m";
 const yellow = (str) => yellowBoldTermPrefix + str + resetStyle;
 
-
-const args = Bun.argv.slice(2)?.[0]
+const args = Bun.argv.slice(2)?.[0];
 if (!args) {
-  console.error("No file specified")
-  process.exit(1)
+  console.error("No file specified");
+  process.exit(1);
 }
 
 const file = Bun.file(args);
 const source = await file.text();
 
-console.log(yellow(" ⚡ Compiling Chicory Source ⚡"))
-const {code, errors} = compile(source) || {code: "", errors: []}
-errors.forEach((error,index) => {
-  console.error(`\nError ${index}:\n${JSON.stringify(error.range.start)}\n${JSON.stringify(error.message)}\n`)
-})
- 
-console.log(yellow(" ⚡ Executing ⚡"))
+console.log(yellow(" ⚡ Compiling Chicory Source ⚡"));
+const { code, errors } = compile(source) || { code: "", errors: [] };
+errors.forEach((error, index) => {
+  console.error(
+    `\nError ${index}:\n${JSON.stringify(error.range.start)}\n${JSON.stringify(
+      error.message
+    )}\n`
+  );
+});
+
+console.log(yellow(" ⚡ Executing ⚡"));
 
 // run the compiled code:
 const proc = Bun.spawn(["bun", "run", "-"], {
   stdin: "pipe",
-  stdout: "inherit"
-})
-proc.stdin.write(code)
-proc.stdin.end()
+  stdout: "inherit",
+});
+proc.stdin.write(code);
+proc.stdin.end();
