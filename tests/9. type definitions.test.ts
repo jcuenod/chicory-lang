2fb97ec398f4b55d96374b948318e4f484c72d44
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-03-02 14:09:51 -0600

    Fix names of type definition tests

--- tests/9. type definitions.test.ts
+++ tests/9. type definitions.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from "bun:test";
 import compile from "../compile";
 
-test("let assign number", () => {
+test("primitive type", () => {
   const { code } = compile(`type MyPrimitiveType = string`);
   expect(code).toBe(``);
 });
 
-test("let assign number", () => {
+test("record type", () => {
   const { code } = compile(`type MyRecord = {
   name: str,
   age: int,
@@ -14,20 +14,21 @@ test("let assign number", () => {
   expect(code).toBe(``);
 });
 
-test("let assign number", () => {
+test("tuple type", () => {
   const { code } = compile(`type MyTuple = [int, str, Person]`);
   expect(code).toBe(``);
 });
 
-test("let assign number", () => {
+test("enum type", () => {
   const { code } = compile(`type MyEnum = Option1 | Option2 | Option3`);
   expect(code).toBe(``);
 });
 
-test("let assign number", () => {
+test("adt type", () => {
   const { code } = compile(`type MyAdt = 
     | ValueA(string)
     | ValueB({width: number, height: number})
+    | ValueC(SomeType)
     | ValueD`);
   expect(code).toBe(``);
 });
